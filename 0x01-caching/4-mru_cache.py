07c02ddda176ba7fa62641be6ce441d18a1772e3
elcis2614 <elisereflorbusole@gmail.com>
2023-10-26 00:06:36 +0000

    mru cache

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+    LIFO caching class
+"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+        MRU cache class
+        inherits form BaseCaching
+    """
+    def __init__(self):
+        super().__init__()
+        self._most_key = None
+
+    def put(self, key, item):
+        """
+            assign to the dictionary self.cache_data
+            the item value for the key key
+        """
+        if item and key is not None:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                if key not in self.cache_data:
+                    print("DISCARD: {}".format(self._most_key))
+                    del self.cache_data[self._most_key]
+            self.cache_data[key] = item
+            self._most_key = key
+
+    def get(self, key):
+        """
+            Returns the value in self.cache_data linked to key
+        """
+        try:
+            value = self.cache_data[key]
+            self._most_key = key
+            return value
+        except KeyError:
+            return None
