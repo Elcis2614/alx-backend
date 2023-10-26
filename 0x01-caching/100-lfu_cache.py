7716aed8ee42d4980e42833dcbb951ce6d592908
elcis2614 <elisereflorbusole@gmail.com>
2023-10-26 02:57:58 +0000

    lfu cache

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -19,7 +19,7 @@ class LFUCache(BaseCaching):
             assign to the dictionary self.cache_data
             the item value for the key key
         """
-        if key and item is None:
+        if key is None or item is None:
             return
         if key in self.cache_data:
             self.cache_data[key] = item
@@ -32,7 +32,7 @@ class LFUCache(BaseCaching):
                 leastF = 999999
                 leastKey = 0
                 for lkey, value in self._keys.items():
-                    if value <= leastF:
+                    if value < leastF:
                         leastKey = lkey
                         leastF = value
                 del self._keys[leastKey]
