8a6f6e1ac3733e048e0ba7b82d51d41d58546fad
elcis2614 <elisereflorbusole@gmail.com>
2023-11-03 15:04:23 +0000

    easy app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,13 +2,15 @@
 """
    Basic Flask app
 """
-from flask import Flask
-from flask import render_template
+from flask import (
+    Flask,
+    render_template,
+)
 
 app = Flask(__name__)
 
 
-@app_view.route('/')
+@app.route('/')
 def simple():
     """
         Returns a simple template
