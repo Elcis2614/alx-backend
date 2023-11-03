02f5e98146a25205b42a7d4aec10d242267851bc
elcis2614 <elisereflorbusole@gmail.com>
2023-11-03 21:44:36 +0000

    get local from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -25,9 +25,15 @@ class Config(object):
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    """ determine the best match with our supported languages."""
+    return babel.localeselector.best_match(Config.LANGUAGES)
+
+
 @app.route('/')
 def simple():
     """
         Returns a simple template
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
