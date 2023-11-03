3ed2402e4b3d53bd453aefdc3f41792ae5ffada7
elcis2614 <elisereflorbusole@gmail.com>
2023-11-03 21:32:32 +0000

    basic babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -12,18 +12,18 @@ from flask import (
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config(object):
     """
         COnfigure available languages
     """
     LANGUAGES = ["en", "fr"]
+    DEFAULT_ZONE = "UTC"
 
-@babel.localeselector
-def get_local():
-    """
-        gets local
-    """
-    return Config.LANGUAGES[0]
+
+babel.BABEL_DEFAULT_LOCALE = Config.LANGUAGES[0]
+babel.BABEL_DEFAULT_TIMEZONE = Config.DEFAULT_ZONE
+
 
 @app.route('/')
 def simple():
